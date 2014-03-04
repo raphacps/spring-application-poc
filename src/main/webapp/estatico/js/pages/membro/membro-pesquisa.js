4b6a3f6d1e2c76274072f52258b1cfc9c1d65133
Raphael Soares <raphacps@gmail.com>
2014-03-04 15:50:19 -0300

    Configurando datatable para membros

---
+++ src/main/webapp/estatico/js/pages/membro/membro-pesquisa.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+	page.init();
+});
+
+var page = {
+	init: function(){
+		page.limparFormulario();
+	},
+	
+	exibeResultado : function(){
+		$(function() {
+		    $('#membroTable').dataTable({
+		    	"oLanguage": application.DATATABLE_I18N_CONFIGURATIONS,
+		    	"bProcessing": true,
+		    	"bRetrieve": true,
+		        "sAjaxSource": 'consultarMembros',
+		        "fnServerParams": function ( aoData ) {
+		        	$($("form").serializeArray()).each(function(){
+						aoData.push( {"name": this.name, "value": this.value} );
+					});
+		        },
+		        "aoColumns": [
+		                      { "mData": "nome" },
+		                      { "mData": "idade" },
+		                      { "mData": "cpf" },
+		                      { "mData": "rg" },
+		                      { "mData": null, "mRender": function( data, type, full) {
+															return "<div class='visible-md visible-lg hidden-sm hidden-xs action-buttons'>" +
+																		"<a class='green'  href='prepararAlteracao?id=" + full.id + "'>" +
+																			"<i class='icon-pencil bigger-130'></i>" +
+																		"</a>" +
+																
+																		"<a class='red'  href='removerMembro?id=" + full.id + "'>" +
+																			"<i class='icon-trash bigger-130'></i>" +
+																		"</a>" +
+																	"</div>";
+								                     }
+							}
+		                  ]
+		    });
+		})
+	},
+	
+	limparFormulario : function(){
+		$('#tableResult').hide();
+	},
+	
+	esconderFormulario : function(){
+		$('#corpoFormulario').toggle('slow');
+	}
+}
+
+$( "#btnPesquisar" ).click(function() {
+  $( "#tableResult" ).show("slow",page.exibeResultado);
+});
+
+$( "#cabecalhoFormulario" ).click(function() {
+	page.esconderFormulario();
+});
+
+$( "#btnLimpar" ).click(page.limparFormulario);
+
+
